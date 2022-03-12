418b2da7be702fbd979d9e42802898c5a0681fc1
Isaac-kiptoo-kilimo <isaac.kilimo@student.moringaschool.com>
2022-03-12 22:20:22 +0300

    feature:index.js file

--- js/index.js
+++ js/index.js
@@ -1,16 +1,16 @@
-// $(document).ready(()=>{
-//     $('#img1').on('click',(event)=>{
-//         event.preventDefault()
-//             $( "#img1" ).hide(
-//             function() {
-//                 $( this ).addClass( "selected" );
-//             }, function() {
-//                 $( this ).removeClass( "description" );
-//             }
-//             );
-//         // $(this).each((event)=>{
-//         //     $('#img1').hide()
-//         //     $('#img1').siblings().add(d-block)
-//         // })
-//     })
-// })
+
+$('.img').click((e)=>{
+     e.currentTarget.nextElementSibling.classList.remove('d-none')
+     e.currentTarget.nextElementSibling.classList.add('d-block')
+     $('.description').css({'text-align':'center'})
+ 
+})
+$('.por').mouseenter((e)=>{
+    console.log($(this))
+    $(e.currentTarget).find('.title').addClass('d-block')
+    $(e.currentTarget).find('.title').removeClass('d-none')
+})
+$('.por').mouseleave((e)=>{
+    $(e.currentTarget).find('.title').addClass('d-none')
+    $(e.currentTarget).find('.title').removeClass('d-block')
+})
--- js/script.js
+++ js/script.js
@@ -8,30 +8,44 @@
 
  
 
-image.addEventListener('click',(e)=>{
-    e.preventDefault()
+// image.addEventListener('click',(e)=>{
+//     e.preventDefault()
     
-    image.classList.add('d-none')
-    description1.classList.remove('d-none')
+//     image.classList.add('d-none')
+//     description1.classList.remove('d-none')
   
 
-})
+// })
 
 
-image2.addEventListener('click',(e)=>{
-    e.preventDefault()
+// image2.addEventListener('click',(e)=>{
+//     e.preventDefault()
     
-    image2.classList.add('d-none')
-    description2.classList.remove('d-none')
-
-    
-})
-image3.addEventListener('click',(e)=>{
-    e.preventDefault()
-    
-    image3.classList.add('d-none')
-    description3.classList.remove('d-none')
+//     image2.classList.add('d-none')
+//     description2.classList.remove('d-none')
 
     
-})
+// })
+// image3.addEventListener('click',(e)=>{
+//     e.preventDefault()
+    
+//     image3.classList.add('d-none')
+//     description3.classList.remove('d-none')
 
+    
+// })
+
+const images=document.querySelectorAll('.img');
+for(let i=0;i<images.length; i++){
+    images[i].addEventListener('click',(e)=>{
+            // e.preventDefault()
+            
+            // images[i].classList.add('d-none')
+            // description3.classList.remove('d-none')
+            // images[i].(".description").classList.add('d-block')
+             images[i].nextElementSibling.classList.remove('d-none')
+             images[i].nextElementSibling.classList.add('d-block')
+        
+            
+        })
+}
