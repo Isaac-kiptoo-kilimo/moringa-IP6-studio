f960912b4c69edf3da2bd548c8758007dd531c6b
Isaac-kiptoo-kilimo <isaac.kilimo@student.moringaschool.com>
2022-03-12 08:47:30 +0300

    bug:This will fix a bug on the event listeners

---
+++ js/index.js
@@ -0,0 +1,16 @@
+// $(document).ready(()=>{
+//     $('#img1').on('click',(event)=>{
+//         event.preventDefault()
+//             $( "#img1" ).hide(
+//             function() {
+//                 $( this ).addClass( "selected" );
+//             }, function() {
+//                 $( this ).removeClass( "description" );
+//             }
+//             );
+//         // $(this).each((event)=>{
+//         //     $('#img1').hide()
+//         //     $('#img1').siblings().add(d-block)
+//         // })
+//     })
+// })
