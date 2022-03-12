2ab171695ecc46eb3fa20b7217b1f0725d00c739
Isaac-kiptoo-kilimo <isaac.kilimo@student.moringaschool.com>
2022-03-12 07:22:27 +0300

    fix:added new description to design and product icons

--- js/script.js
+++ js/script.js
@@ -1,13 +1,14 @@
-const image=document.querySelector('#img1')
-const description1=document.querySelector('#description1')
-const description2=document.querySelector('#description2')
-const description3=document.querySelector('#description3')
-const hoverme=document.querySelector('.hoverme')
-const hoverme2=document.querySelector('.hoverme2')
-const image2=document.querySelector('#img2')
-const image3=document.querySelector('#img3')
+ const image=document.querySelector('#img1')
+ const image2=document.querySelector('#img2')
+ const image3=document.querySelector('#img3')
+ const description1=document.querySelector('#description1')
+ const description2=document.querySelector('#description2')
+ const description3=document.querySelector('#description3')
 
-hoverme.addEventListener('click',(e)=>{
+
+ 
+
+image.addEventListener('click',(e)=>{
     e.preventDefault()
     
     image.classList.add('d-none')
@@ -34,29 +35,3 @@ image3.addEventListener('click',(e)=>{
     
 })
 
-
-
-
-
-// $(document).ready(function(){
-//     $('.img').click(()=>{
-//        $(this).find('.description').addClass('d-block')
-//        console.log($(this))
-//     })
-// // })
-// $(document).ready(function(){
-//     $('.icons').click(
-//     function(){
-//         $(this).siblings().next('d-block').hide();
-//     },
-//     function(){
-//         $(this).siblings().next('.description').show('slow');
-//     });
-// });
-// $('.img').each(()=>{
-//     $(this).click(()=>{
-//         // console.log('clicked')
-//         $(this).
-//     })
-
-// })
