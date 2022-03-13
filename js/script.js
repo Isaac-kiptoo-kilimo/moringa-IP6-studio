c5ee4c70daf965c620127d5eea86b97aceb89581
Isaac-kiptoo-kilimo <isaac.kilimo@student.moringaschool.com>
2022-03-13 19:08:08 +0300

    fixing the jQuery part

--- js/script.js
+++ js/script.js
@@ -1,51 +1,39 @@
- const image=document.querySelector('#img1')
- const image2=document.querySelector('#img2')
- const image3=document.querySelector('#img3')
- const description1=document.querySelector('#description1')
- const description2=document.querySelector('#description2')
- const description3=document.querySelector('#description3')
+//  const image=document.querySelector('#img1')
+//  const image2=document.querySelector('#img2')
+//  const image3=document.querySelector('#img3')
+//  const description1=document.querySelector('#description1')
+//  const description2=document.querySelector('#description2')
+//  const description3=document.querySelector('#description3')
 
 
- 
-
-// image.addEventListener('click',(e)=>{
-//     e.preventDefault()
-    
-//     image.classList.add('d-none')
-//     description1.classList.remove('d-none')
-  
-
-// })
 
 
-// image2.addEventListener('click',(e)=>{
-//     e.preventDefault()
-    
-//     image2.classList.add('d-none')
-//     description2.classList.remove('d-none')
-
-    
-// })
-// image3.addEventListener('click',(e)=>{
-//     e.preventDefault()
-    
-//     image3.classList.add('d-none')
-//     description3.classList.remove('d-none')
-
-    
-// })
-
-const images=document.querySelectorAll('.img');
-for(let i=0;i<images.length; i++){
-    images[i].addEventListener('click',(e)=>{
-            // e.preventDefault()
+// const images=document.querySelectorAll('.img');
+// for(let i=0;i<images.length; i++){
+//     images[i].addEventListener('click',(e)=>{
+//             // e.preventDefault()
             
-            // images[i].classList.add('d-none')
-            // description3.classList.remove('d-none')
-            // images[i].(".description").classList.add('d-block')
-             images[i].nextElementSibling.classList.remove('d-none')
-             images[i].nextElementSibling.classList.add('d-block')
+//             // images[i].classList.add('d-none')
+//             // description3.classList.remove('d-none')
+//             // images[i].(".description").classList.add('d-block')
+//              images[i].nextElementSibling.classList.remove('d-none')
+//              images[i].nextElementSibling.classList.add('d-block')
         
             
+//         })
+// }
+$(document).ready(function(){
+        $('.design').click(function(){
+                $('#imgdesign').slideToggle();
+                $('#pdesign').slideToggle();
         })
-}
+        $('.design').click(function(){
+                $('#imgdesign').slideToggle();
+                $('#pdesign').slideToggle();
+        })
+
+        $('.title').hide();
+        $('.por.hover').hover(function(){
+                $(this).find('.title').toggle();
+        })
+})
